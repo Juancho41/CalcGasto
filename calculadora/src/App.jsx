e8f16858bad218fe0a7aa1841507b8fd0e793d34
Ariadna Nasisi <ari.nasisi@gmail.com>
2023-06-28 00:21:40 -0300

    se agregan botones de edicion y borrado de ingresos y gastos

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -120,8 +120,8 @@ function App() {
           setBilleterasUsuario={setBilleterasUsuario} usuario={usuario}
           setGastosUsuario={setGastosUsuario} gastosUsuario={gastosUsuario}
           ingresosUsuario={ingresosUsuario} setIngresosUsuario={setIngresosUsuario} />} />
-        <Route path="/ingresos" element={<ListaIngresos ingresosUsuario={ingresosUsuario} />} />
-        <Route path="/gastos" element={<ListaGastos gastosUsuario={gastosUsuario} />} />
+        <Route path="/ingresos" element={<ListaIngresos ingresosUsuario={ingresosUsuario} setIngresosUsuario={setIngresosUsuario} />} />
+        <Route path="/gastos" element={<ListaGastos gastosUsuario={gastosUsuario} setGastosUsuario={setGastosUsuario}/>} />
         <Route path="/newuser" element={<NewUser />} />
       </Routes>
 
